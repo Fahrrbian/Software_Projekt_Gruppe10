2d41ddf03820a8df721d1c9e997913cd11dbf1b8
Fabian Holtapel <holtapelf@gmail.com>
2025-04-27 20:35:51 +0200

    Initial project setup

---
+++ Online_Testat/src/main/java/com/testat/proj/OnlineTestatApplication.java
@@ -0,0 +1,13 @@
+package com.testat.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTestatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTestatApplication.class, args);
+	}
+
+}
---
+++ Online_Testat/src/test/java/com/testat/proj/OnlineTestatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testat.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTestatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
