f7f61c0d74a7eff2bb7657fece71c3752a150b00
Fabian Holtapel <holtapelf@gmail.com>
2025-04-29 15:23:23 +0200

    added Security Button in MainView into Header

--- Online_Testat/src/main/java/com/testat/proj/app/views/MainLayout.java
+++ Online_Testat/src/main/java/com/testat/proj/app/views/MainLayout.java
@@ -1,5 +1,21 @@
 package com.testat.proj.app.views;
 
-public class MainLayout {
+import com.vaadin.flow.component.applayout.AppLayout;
 
-}
+public class MainLayout extends AppLayout{
+
+	public MainLayout(Se) {
+		createHeader(); 
+		createDrawer(); 
+	}
+
+	private void createDrawer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createHeader() {
+		// TODO Auto-generated method stub
+		
+	}
+ }
