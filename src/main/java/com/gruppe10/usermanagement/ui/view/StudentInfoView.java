323d0fbcccd93699be5ca94e4e3ec03b00fa15c3
Fabian Holtapel <holtapelf@gmail.com>
2025-05-11 13:02:19 +0200

    Added User Login for Student and Instructor + Redirect to primitive Student/ Instructor ViewÂ with MainLayout to Logout Button

--- src/main/java/com/gruppe10/usermanagement/ui/view/StudentInfoView.java
+++ src/main/java/com/gruppe10/usermanagement/ui/view/StudentInfoView.java
@@ -1,12 +1,19 @@
 //ChristianMarkow
-package com.gruppe10.usermanagement.ui.view;
+package com.gruppe10.base.ui.view;
 
-import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
 
-@Route("student-info")
+
+@Route(value="student", layout = MainLayout.class)
 @PageTitle("Student Info")
-public class StudentInfoView extends Main {
-
+@RolesAllowed("STUDENT")
+public class StudentInfoView extends VerticalLayout {
+    public StudentInfoView() {
+        System.out.println("Student View geladen");
+        add(new H1("Student Info"));
+    }
 }
---
+++ src/main/java/com/gruppe10/usermanagement/ui/view/adminView.java
@@ -0,0 +1,13 @@
+package com.gruppe10.usermanagement.ui.view;
+
+/**
+ * adminView.java
+ *
+ * Created by Fabian Holtapel on 07.05.2025.
+ *
+ * Description:
+ * TODO: Beschreibung einfügen.
+ */
+
+public class adminView {
+}
