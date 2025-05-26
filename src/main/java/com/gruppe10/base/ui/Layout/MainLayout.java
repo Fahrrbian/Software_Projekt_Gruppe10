594e217eb6bcbec2fa778147d15872697cce9fe3
Christian Markow <Christian.Markow@goldbeck.de>
2025-05-26 22:13:49 +0200

    Fixed MainLayout

--- src/main/java/com/gruppe10/base/ui/Layout/MainLayout.java
+++ src/main/java/com/gruppe10/base/ui/Layout/MainLayout.java
@@ -3,20 +3,29 @@ package com.gruppe10.base.ui.Layout;
 import com.gruppe10.security.AuthenticatedUser;
 import com.gruppe10.usermanagement.domain.User;
 import com.gruppe10.usermanagement.service.UserService;
-import com.gruppe10.usermanagement.ui.view.InstructorInfoView;
-import com.gruppe10.usermanagement.ui.view.StudentInfoView;
+import com.gruppe10.usermanagement.ui.view.UserInfoView;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.avatar.AvatarVariant;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.menu.MenuConfiguration;
+import com.vaadin.flow.server.menu.MenuEntry;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 
 
@@ -26,97 +35,107 @@ import static com.vaadin.flow.theme.lumo.LumoUtility.*;
  * InitUser.java
  * <p>
  * Created by Fabian Holtapel on 08.05.2025.
+ * Fixed by Christian Markow on 26.05.2025.
  * <p>
  * Description:
  * Standard-Layout
  */
 
-@SuppressWarnings("unused")
 public class MainLayout extends AppLayout {
 
-
     private final UserService userService;
-
     private final AuthenticatedUser authenticatedUser;
-
     protected HorizontalLayout header;
 
     public MainLayout(UserService userService, AuthenticatedUser authenticatedUser) {
         this.userService = userService;
         this.authenticatedUser = authenticatedUser;
         setPrimarySection(AppLayout.Section.DRAWER);
-        createDrawer();
         createHeader();
-        //addToDrawer(createHeader(), new Scroller(createSideNav()), createUserMenu());
+        createDrawer();
     }
 
-    protected void createDrawer() {
-        String username = authenticatedUser.get().map(User::getEmail) // oder getForename() / getSurname()
-                .orElseThrow(() -> new IllegalStateException("Kein Benutzer eingeloggt"));
+    protected void createHeader() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.getStyle().set("cursor", "pointer");
 
+        H2 headerText = new H2("Online-Testat");
+        headerText.addClassNames(
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.Margin.LARGE);
 
-        Button logout = new Button("Log out", e -> {
+        Button logout = new Button("Logout", e -> {
             authenticatedUser.logout();
             UI.getCurrent().getPage().setLocation("/login");
         });
-        logout.addClassName("logout-Button");
+        logout.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        logout.getStyle().set("cursor", "pointer");
+        logout.addClassName(LumoUtility.Margin.Left.AUTO);
 
-        HorizontalLayout user = new HorizontalLayout(new Icon(VaadinIcon.HEADSET), new H3(username));
-        user.addClassName("User-Name");
-
-        VerticalLayout avatarAndName = new VerticalLayout(logout);
-        avatarAndName.addClassNames(LumoUtility.Margin.Top.AUTO, Width.FULL);
-
-        // Create Drawer Content
-        VerticalLayout drawerContent = new VerticalLayout();
-        drawerContent.setSizeFull();
-
-        //Add links depending on role
-        String role = authenticatedUser.get().map(User::getRole).orElseThrow(() -> new IllegalStateException("Keine Rolle"));
-        if ("INSTRUCTOR".equals(role)) {
-            drawerContent.add(new RouterLink("instructor", InstructorInfoView.class));
-        } else if ("STUDENT".equals(role)) {
-            drawerContent.add(new RouterLink("student", StudentInfoView.class));
-        }
-
-        drawerContent.add(avatarAndName);
-
-        Header header = new Header(user);
-        addToDrawer(header, new Scroller(drawerContent));
-    }
-    protected void createHeader() {
-        H1 logo = new H1("Online-Testat");
-        logo.addClassNames(
-                LumoUtility.FontSize.LARGE,
-                LumoUtility.Margin.MEDIUM);
-
-        header = new HorizontalLayout(new DrawerToggle(), logo);
+        header = new HorizontalLayout(toggle, headerText, logout);
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        header.expand(logo); // Expands the logo to fill the space
+        header.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
         header.setWidthFull();
+        header.expand(headerText);
         header.addClassNames(
                 LumoUtility.Padding.Vertical.NONE,
                 LumoUtility.Padding.Horizontal.MEDIUM);
 
         addToNavbar(header);
     }
-        /*        // TODO Replace with real application logo and name
-        var appLogo = VaadinIcon.CUBES.create();
-        appLogo.addClassNames(TextColor.PRIMARY, IconSize.LARGE);
 
-        var appName = new Span("Softwareprojekt Gruppe 10");
-        appName.addClassNames(FontWeight.SEMIBOLD, FontSize.LARGE);
+    protected void createDrawer() {
+        String username = getFullName();
 
-        var header = new Div(appLogo, appName);
-        header.addClassNames(Display.FLEX, Padding.MEDIUM, Gap.MEDIUM, AlignItems.CENTER);
-        return header;
+        //HorizontalLayout userInfo = new HorizontalLayout(new H3(username));
+        HorizontalLayout userInfo = new HorizontalLayout(new H3("Menü"));
+        userInfo.setAlignItems(FlexComponent.Alignment.CENTER);
+        userInfo.getStyle().set("padding-left", "0.5rem");
+        userInfo.getStyle().set("padding-top", "0.3rem");
+        userInfo.getStyle().set("padding-bottom", "1rem");
+
+        //Header userInfoHeader = new Header(userInfo);
+        addToDrawer(userInfo, new Scroller(createSideNav()), createUserInfo());
     }
- Das muss lazy in einer View erzeugt werden
+
     private SideNav createSideNav() {
         var nav = new SideNav();
-        nav.addClassNames(Margin.Horizontal.MEDIUM);
+        nav.addClassNames(Margin.Horizontal.MEDIUM, Margin.Vertical.MEDIUM);
         MenuConfiguration.getMenuEntries().forEach(entry -> nav.addItem(createSideNavItem(entry)));
         return nav;
+    }
+
+    private Component createUserInfo() {
+        String fullName = getFullName();
+
+        var avatar = new Avatar(fullName);
+        avatar.addThemeVariants(AvatarVariant.LUMO_XSMALL);
+        avatar.setColorIndex(5);
+
+        var userName = new Span(fullName);
+
+        var avatarAndUserName = new HorizontalLayout(avatar, userName);
+        avatarAndUserName.setAlignItems(FlexComponent.Alignment.CENTER);
+        avatarAndUserName.setPadding(false);
+        avatarAndUserName.setMargin(false);
+        avatarAndUserName.getStyle().set("padding-left", "0.5rem");
+
+        var userMenu = new MenuBar();
+        userMenu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY_INLINE);
+        userMenu.addClassNames(Margin.MEDIUM);
+
+        var userMenuItem = userMenu.addItem(avatarAndUserName);
+        /*
+        userMenuItem.getSubMenu().addItem("Profil", e -> {
+            UI.getCurrent().getPage().setLocation("/user-info");
+        });
+        userMenuItem.getSubMenu().addItem("Logout", e -> {
+            authenticatedUser.logout();
+            UI.getCurrent().getPage().setLocation("/login");
+        });
+        */
+
+        return userMenu;
     }
 
     private SideNavItem createSideNavItem(MenuEntry menuEntry) {
@@ -127,24 +146,8 @@ public class MainLayout extends AppLayout {
         }
     }
 
-    private Component createUserMenu() {
-        // TODO Replace with real user information and actions
-        var avatar = new Avatar("John Smith");
-        avatar.addThemeVariants(AvatarVariant.LUMO_XSMALL);
-        avatar.addClassNames(Margin.Right.SMALL);
-        avatar.setColorIndex(5);
-
-        var userMenu = new MenuBar();
-        userMenu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY_INLINE);
-        userMenu.addClassNames(Margin.MEDIUM);
-
-        var userMenuItem = userMenu.addItem(avatar);
-        userMenuItem.add("John Smith");
-        userMenuItem.getSubMenu().addItem("View Profile");
-        userMenuItem.getSubMenu().addItem("Manage Settings");
-        userMenuItem.getSubMenu().addItem("Logout");
-
-        return userMenu;
+    private String getFullName() {
+        return authenticatedUser.get().map(user -> user.getForename() + " " + user.getSurname())
+                .orElseThrow(() -> new IllegalStateException("Kein Benutzer eingeloggt"));
     }
-*/
 }
